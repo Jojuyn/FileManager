0400c014ea26b4d8ba44ae80d1ca7086650bb2ee
kychen-bit <3022450332@qq.com>
2025-07-12 17:21:39 +0800

    Initial commit: Set up project structure and add basic files

---
+++ ConfigManager.h
@@ -0,0 +1,37 @@
+//
+// Created by kychen on 25-7-11.
+//
+
+#ifndef CONFIGMANAGER_H
+#define CONFIGMANAGER_H
+#include "Header.h"
+
+class ConfigManager {
+public:
+    //从文件加载配置到内存
+    void loadConfig(const std::string& filePath);
+    //将内存配置写入文件
+    void saveConfig(const std::string& filePath);
+    //获取默认路径
+    std::string getDefaultPath() const;
+    //获取showHidden参数
+    bool getShowHidden()const;
+    //获取历史路径
+    std::vector<std::string> getRecentPaths()const;
+    //添加最近的访问路径
+    void addRecentPath(const std::string& path);
+    //修改默认路径
+    void setDefaultPath(const std::string& path);
+    //设置是否展示隐藏文件 默认为true
+    void setShowHidden(const bool &val);
+
+private:
+    std::string defaultPath;
+    std::vector<std::string> recentPaths;
+    bool showHidden=true;
+    nlohmann::json Config;
+};
+//测试用例
+void test01();
+void test02();
+#endif //CONFIGMANAGER_H
