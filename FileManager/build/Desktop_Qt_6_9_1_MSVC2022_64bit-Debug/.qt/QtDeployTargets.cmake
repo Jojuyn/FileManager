55d991f2fdf705300906d4a35e7355e84259a9a5
Jojuyn <2452337514@qq.com>
2025-07-16 20:46:55 +0800

    架构分支初始化

---
+++ FileManager/build/Desktop_Qt_6_9_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_FileManager_FILE D:/Workspace/TuringNinth/FileManager/FileManager/build/Desktop_Qt_6_9_1_MSVC2022_64bit-Debug/FileManager.exe)
+set(__QT_DEPLOY_TARGET_FileManager_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_FileManager_RUNTIME_DLLS D:/Workspace/Application/Qt/6.9.1/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Workspace/Application/Qt/6.9.1/msvc2022_64/bin/Qt6Guid.dll;D:/Workspace/Application/Qt/6.9.1/msvc2022_64/bin/Qt6Cored.dll)
