57063d2327eb44f0cea7a4834b10b32466943381
Jojuyn <2452337514@qq.com>
2025-07-18 07:59:54 +0800

    与异常处理分支合并

--- FileManager/ExceptionHandler.h
+++ FileManager/ExceptionHandler.h
@@ -9,17 +9,19 @@
 
 using namespace std;
 
+namespace Exception {
+
 class ExceptionHandler {
 public:
     static void handleException(const exception& e);
 };
 
+constexpr string_view log_path = "";
+
 class Logger
 {
 public:
     static void log(const string& message);
-private:
-    static string logfile = "operation_log.txt";
 };
 
 class InvalidPath : public runtime_error {};
@@ -27,6 +29,10 @@ class InvalidPath : public runtime_error {};
 class FileOccupied : public runtime_error {};
 
 class NoPermission : public runtime_error {};
+
+};
+
+
 
 
 
